b917b5159c614263d1fe4132fd148efcba6021ce
Sam Jaques <s.jaques@televic.com>
2024-12-16 23:38:43 +0100

    feat: add `if let Some(ref ...)` to option example

--- examples/option.rs
+++ examples/option.rs
@@ -1,13 +1,19 @@
 // An integer division that doesn't `panic!`
-fn checked_division(dividend: i32, divisor: i32) -> Option<i32> {
+// Extra box to prove `ref` keyword below
+fn checked_division(dividend: i32, divisor: i32) -> Option<Box<i32>> {
     if divisor == 0 {
         None
     } else {
-        Some(dividend / divisor)
+        Some(Box::new(dividend / divisor))
     }
 }
 
 fn main() {
     let d = checked_division(0, 1);
-    d.expect("division failed");
+
+    if let Some(ref number) = d {
+        println!("Number is {number}");
+    }
+
+    println!("Number is {}", d.expect("not null"));
 }
